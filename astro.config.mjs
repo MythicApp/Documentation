fca780a83a551932ef38030bc420704506e547d1
Jeremy <68130184+jeremybosma@users.noreply.github.com>
2024-06-12 09:43:05 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.getmythic.app',
 	integrations: [
 		starlight({
 			title: 'My Docs',
